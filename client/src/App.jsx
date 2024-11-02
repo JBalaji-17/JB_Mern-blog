e9e8c529af7de113d452f83785e9eb2451e037d3
Testing-at-811779193268 <jeyabalaji.p@pruvity.org>
2024-11-02 12:25:18 +0530

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { BrowserRouter , Routes, Route } from "react-router-dom"
+import home from './pages/home'
+import about from "./pages/about"
+import signin from "./pages/signin"
+import signup from "./pages/signup"
+import project from "./pages/project"
+import dashboard from "./pages/dashboard"
 
 export default function App() {
   return (
-    <h1 className='text-3xl text-red-500'>App</h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<home/>} />
+      <Route path="/about" element={<about />} />
+      <Route path="/signin" element={<signin />} />
+      <Route path="/signup" element={<signin />} />
+      <Route path="/dashboard" element={<dashboard />} />
+      <Route path="/project" element={<project />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
